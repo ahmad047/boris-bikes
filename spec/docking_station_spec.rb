23d3ebb25c716954ad8751c5fa0aeee0d2b708f8
Muhammad Nasr Ahmad <nasrahmadcheema@hotmail.com>
2021-04-01 09:57:41 +0100

    test for docking bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,15 @@
 require "docking_station"
-
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-end
+
+  it { is_expected.to respond_to :docking }
+
+  it 'docks a bike to the station' do
+    bike = Bike.new
+    expect(subject.docking(bike)).to eq(bike)
+  end
+end 
