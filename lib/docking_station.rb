28c259d5a2d0fb3f2108073a741b3d34b718ad5a
Muhammad Nasr Ahmad <nasrahmadcheema@hotmail.com>
2021-04-01 14:46:38 +0100

    challenge 14 done
    Co-authored-by: Muhammad Nasr Ahmad <nasrahmadcheema@hotmail.com>
    Co-authored-by: Phil Smithies <philsmithies@gmail.com>

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,18 @@
 require_relative "bike"
 class DockingStation
-  attr_reader :bike
-  def release_bike
-    fail 'there are no bikes' unless @bike
-    @bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
   end
 
-  def docking(bike)
-    fail 'station is full' if @bike
-    @bike = bike
+  def release_bike
+    fail 'there are no bikes' if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail 'station is full' if @bikes.length >= 20
+    @bikes.push(bike)
   end
 end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,19 +8,21 @@ describe DockingStation do
       expect(bike).to be_working
     end
     it 'return an error if there are no bikes' do
+      @bikes = []
       expect { subject.release_bike }.to raise_error 'there are no bikes'
     end
   end
 
   describe '#docking' do
-    it { is_expected.to respond_to :docking }
+    it { is_expected.to respond_to :dock }
     it 'docks a bike to the station' do
       bike = Bike.new
-      expect(subject.docking(bike)).to eq(bike)
+      expect(subject.dock(bike)).to eq([bike])
     end
     it 'return an error if docking station is full' do
-      subject.docking(Bike.new)
-      expect { subject.docking(Bike.new) }.to raise_error 'station is full'
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error 'station is full'
     end
+    
   end 
 end
